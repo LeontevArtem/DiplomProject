1c66f523d4344fad743f11e77e48b22dcc95f9e1
Артём Леонтьев <artemleont03@gmail.com>
2024-05-11 19:31:21 +0500

    Чуть улучшил поиск

--- WorkplacesAccounting/Controllers/SessionController.cs
+++ WorkplacesAccounting/Controllers/SessionController.cs
@@ -12,9 +12,7 @@ namespace WorkplacesAccounting.Controllers
         [Authorize]
         public IActionResult Index(string id)
         {
-            //Data.LoadData();
             SessionModel model = new SessionModel();
-            //Data.LoadData();
             model.Session = Data.SessionsList.Find(x => x.ID==Convert.ToInt32(id));
             model.Logs = Data.LogList.Where(x => x.Session==model.Session).ToList();
             model.ObservationsList = Data.ObservationsList.Where(x =>x.Session.ID==model.Session.ID).ToList();
@@ -24,9 +22,6 @@ namespace WorkplacesAccounting.Controllers
         [Authorize]
         public IActionResult Test(string id)
         {
-            //Debug.WriteLine(id
-            //Response.WriteAsync("<script>alert('Your text');</script>");
-            //Session curSession = Data.SessionsList.Find(x =>x.ID.ToString()==id);
             System.Data.DataTable Insert = MsSQL.Query($"UPDATE [dbo].[Sessions] SET [EndTime] = '{DateTime.Now}' WHERE SessionID = '{id}' ", Data.ConnectionString);
 
             return RedirectToAction("Index", new { id });
